5d5ecc9b71da1205dfa1b912846d3aaf27f39a17
Washieu Anan <washieu05@gmail.com>
2023-01-21 12:07:50 -0500

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeServoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/IntakeServoTesting.java
@@ -14,7 +14,14 @@ public class IntakeServoTesting extends LinearOpMode {
     public Servo leftClaw;
     public Servo rotation;
 
-    public static double ROTATION_SERVO_POSITION = 0;
+    public static double ROTATION_INTAKE = 0;
+    public static double ROTATION_OUTTAKE = 1;
+
+    public static double CLAW_OPEN = 0.5;
+    public static double CLAW_CLOSE = 0.9;
+
+    public static double ARM_UP = 1;
+    public static double ARM_DOWN = 0;
 
     @Override
     public void runOpMode(){
@@ -27,25 +34,30 @@ public class IntakeServoTesting extends LinearOpMode {
 
         while(opModeIsActive()){
 
-            if(gamepad1.dpad_up){
-                setArm(0.5);
-            }
-
-            if(gamepad1.dpad_down){
-                setArm(0.7);
-            }
-
-            if(gamepad1.dpad_left){
-                setArm(1);
-            }
-
-            if(gamepad1.dpad_right){
-                setArm(0);
-            }
 
 
             if(gamepad1.a){
-                rotation.setPosition(ROTATION_SERVO_POSITION);
+                rotation.setPosition(ROTATION_INTAKE);
+            }
+
+            if(gamepad1.b){
+                rotation.setPosition(ROTATION_OUTTAKE);
+            }
+
+            if(gamepad1.x){
+                leftClaw.setPosition(CLAW_OPEN);
+            }
+
+            if(gamepad1.y){
+                leftClaw.setPosition(CLAW_CLOSE);
+            }
+
+            if(gamepad1.dpad_up){
+                setArm(ARM_UP);
+            }
+
+            if(gamepad1.dpad_down){
+                setArm(ARM_DOWN);
             }
 
 
